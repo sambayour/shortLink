6763a79a40c72fc069b9fa4669d50a7b92aa5f9f
Samuel Olubayo <102560992+sambayo@users.noreply.github.com>
2023-04-23 01:54:07 +0100

    swagger api docs

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const port = process.env.PORT ? Number(process.env.PORT) : 2023;
 
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Short Link')
+    .setDescription('API to encode and decode URL.')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
   await app.listen(port);
   console.log('App started on', port);
 }
